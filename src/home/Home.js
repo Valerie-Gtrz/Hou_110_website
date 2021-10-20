4b89118715867989b3fc05d22817a41b68b412f9
Valerie Gutierrez <Valeriengutierrez@yahoo.com>
2021-10-19 22:51:12 -0500

    made img dynamic added card to home

--- src/home/Home.js
+++ src/home/Home.js
@@ -3,25 +3,22 @@ import WhoWeAre from "../data/whoWeAre";
 
 function Home() {
   return (
-    <>
-      <div className="d-flex flex-row bd-highlight justify-content-start mb-3">
-        
-          <div>
-            <img
-              src={cleanup}
-              alt="jesse h. jones park clean up"
-              width="650"
-              height="500"
-              className="mx-4"
-            />
-          </div>
+     
 
-          <div>
-            <WhoWeAre />
-          </div>
-        
+<div className="card mb-3 width-50">
+  <div className="row g-0">
+    <div className="col-md-4">
+    <img src={cleanup} alt="jesse h. jones park clean up" />
+    </div>
+    <div className="col-md-8">
+      <div className="card-body">
+        <h5 className="card-title">Who We Are</h5>
+        <p className="card-text"><WhoWeAre /></p>
+        <p className="card-text"><small className="text-muted">Jesse H. Jones park clean up</small></p>
       </div>
-    </>
+    </div>
+  </div>
+</div>
   );
 }
 
