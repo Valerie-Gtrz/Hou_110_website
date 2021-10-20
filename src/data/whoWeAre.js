e5c39abe46cf9210fead8df9f99e3a8c7499fd0b
Valerie Gutierrez <Valeriengutierrez@yahoo.com>
2021-10-19 21:35:07 -0500

    replaced container with flexbox for homepage

--- src/data/whoWeAre.js
+++ src/data/whoWeAre.js
@@ -1,6 +1,6 @@
 function WhoWeAre(){ 
     return(
-<p>My Text goes here</p>
+<p>Quisque condimentum nulla at ante consectetur, eu pellentesque nunc vulputate. Suspendisse facilisis congue iaculis. Duis ac sollicitudin leo, sit amet ultricies felis. Maecenas placerat leo eget efficitur blandit. Nulla ante orci, aliquam sagittis mauris in, suscipit luctus risus. Integer eu condimentum mauris, et consequat felis. Aenean egestas ultrices ipsum congue dictum. Curabitur nec est lacinia urna dapibus auctor. Integer condimentum nec libero in hendrerit. Sed pulvinar, ex et convallis finibus, turpis sapien imperdiet elit, sit amet commodo tellus mi nec elit. Cras ultrices magna in nisl cursus sollicitudin. Aliquam a fermentum orci. Curabitur tempus felis ut odio viverra, vitae rutrum felis dictum. Integer malesuada porttitor magna. Proin metus felis, commodo nec tincidunt ut, condimentum quis purus. Donec rhoncus turpis ac maximus aliquam.</p>
     )
 }
 
--- src/home/Home.js
+++ src/home/Home.js
@@ -1,23 +1,28 @@
-import cleanup from "../images/cleanup.jpg"
+import cleanup from "../images/cleanup.jpg";
 import WhoWeAre from "../data/whoWeAre";
 
+function Home() {
+  return (
+    <>
+      <div className="d-flex flex-row bd-highlight justify-content-start mb-3">
+        
+          <div>
+            <img
+              src={cleanup}
+              alt="jesse h. jones park clean up"
+              width="650"
+              height="500"
+              className="mx-4"
+            />
+          </div>
 
-function Home(){
-    return(
-<>
-<div className="container mt-5">
-    <div className="row">
-    <div className="col">
-<img src={cleanup} alt="jesse h. jones park clean up" width="650" height="500"/>
-</div>
-
-
-<div className="col"><WhoWeAre /></div>
-</div>
-</div>
-
-</>
-    )
-};
+          <div>
+            <WhoWeAre />
+          </div>
+        
+      </div>
+    </>
+  );
+}
 
 export default Home;
