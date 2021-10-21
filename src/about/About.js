508854b1861677a0b375bf87f6694e015e7de3b1
Valerie Gutierrez <Valeriengutierrez@yahoo.com>
2021-10-21 14:09:56 -0500

    adjusted hr margins in header

--- src/about/About.js
+++ src/about/About.js
@@ -1,26 +1,25 @@
 import WhoWeAre from "../data/whoWeAre";
 import cleanup from "../images/cleanup.jpg";
 
-function About(){
-    return(
-        <>
-        
-         <div class="container">
-             <div className="row">
-             <div className="col-lg-6 my-3">
-    <img src={cleanup} alt="" />
-    </div>
+function About() {
+  return (
+    <>
+      <div class="container">
+        <div className="row">
+          <div className="col-lg-6 my-3">
+            <img src={cleanup} alt="" />
+          </div>
 
-  <div className="col-lg-5 my-3">
-      <h5 class="card-title">Card title that wraps to a new line</h5>
-      <p class="card-text"><WhoWeAre /></p>
-    </div>
-    </div>
-    </div>
- 
+          <div className="col-lg-5 my-3">
+            <h5 class="card-title">Card title that wraps to a new line</h5>
+            <p class="card-text">
+              <WhoWeAre />
+            </p>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
 
-        </>
-    )
-    }
-
-    export default About;
+export default About;
